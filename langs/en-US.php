0c0b18953afcfeea4d50f051fa1bd2d3a4a53032
jocafamaka <joao_carlos.r@hotmail.com>
2018-10-26 23:51:04 -0300

    New validations and startup checks | Correction
    The following changes were made:
    - New validation structure, now all errors are displayed instead of only the first one.
    - New validators for API, user and password selection.
    - Fixes bug created in commit b6885b0.

--- langs/en-US.php
+++ langs/en-US.php
@@ -29,6 +29,8 @@ $reporterErrorPre =("An error has occurred but could not be reported!");
 
 $reporterError =("This version of NagMap Reborn is no longer supported for bug fixes. Please use the <a href='https://github.com/jocafamaka/nagmapReborn/releases'>latest available version</a>.");
 
+$emptyUserPass = ("Authentication username and/or password have not been defined, set user and password in the configuration file.");
+
 //Debug info:
 $message = ("Message:");
 
--- validateAndVerify.php
+++ validateAndVerify.php
@@ -1,5 +1,9 @@
 <?php
 // Check if the config file exist.
+
+$fails = '';
+
+// Check if the config file exist.
 if(file_exists("config.php"))
   include('config.php');
 else
@@ -13,79 +17,85 @@ else
 
 // Validation of configuration variables.
 if(!is_string($nagios_cfg_file)) 
-  die("\$nagios_cfg_file $var_cfg_error ($nagios_cfg_file)");
+  $fails = "<b>\$nagios_cfg_file</b> $var_cfg_error ($nagios_cfg_file)";
 
 if(!is_string($nagios_status_dat_file)) 
-  die("\$nagios_status_dat_file $var_cfg_error ($nagios_status_dat_file)");
+  $fails .= "<br><b>\$nagios_status_dat_file</b> $var_cfg_error ($nagios_status_dat_file)";
 
-if(($nagMapR_MapAPI < 0) || ($nagMapR_MapAPI > 1))
-  die("\$nagMapR_MapAPI $var_cfg_error ($nagMapR_MapAPI)");
+if(($nagMapR_MapAPI < 0) || ($nagMapR_MapAPI > 1) || (!isset($nagMapR_MapAPI)))
+  $fails .= "<br><b>\$nagMapR_MapAPI</b> $var_cfg_error ($nagMapR_MapAPI)";
 
 if($nagMapR_MapAPI == 0){
 
-  if(!is_string($nagMapR_Mapkey) || empty($nagMapR_Mapkey)) 
-    die("\$nagMapR_Mapkey $var_cfg_error ($nagMapR_Mapkey)");
+  if(!is_string($nagMapR_Mapkey) || !isset($nagMapR_Mapkey)) 
+    $fails .= "<br><b>\$nagMapR_Mapkey</b> $var_cfg_error ($nagMapR_Mapkey)";
 
   if(!is_string($nagMapR_MapType)) 
-    die("\$nagMapR_MapType $var_cfg_error ($nagMapR_MapType)");
+    $fails .= "<br><b>\$nagMapR_MapType</b> $var_cfg_error ($nagMapR_MapType)";
 
   if($nagMapR_Style != ''){
     if(!file_exists("styles/$nagMapR_Style.json")){
-      die("\$nagMapR_Style $var_cfg_error ($nagMapR_Style)");
+      $fails .= "<br><b>\$nagMapR_Style</b> $var_cfg_error ($nagMapR_Style)";
     }
   }
 }
 
 if(!is_string($nagMapR_FilterHostgroup)) 
-  die("\$nagMapR_FilterHostgroup $var_cfg_error ($nagMapR_FilterHostgroup)");
+  $fails .= "<br><b>\$nagMapR_FilterHostgroup</b> $var_cfg_error ($nagMapR_FilterHostgroup)";
 
 if(!is_string($nagMapR_MapCentre)) 
-  die("\$nagMapR_MapCentre $var_cfg_error ($nagMapR_MapCentre)");
+  $fails .= "<br><b>\$nagMapR_MapCentre</b> $var_cfg_error ($nagMapR_MapCentre)";
 
-if(!is_string($nagMapR_key)) 
-  die("\$nagMapR_key $var_cfg_error ($nagMapR_key)");
+if((!is_string($nagMapR_key)) || !isset($nagMapR_key)) 
+  $fails .= "<br><b>\$nagMapR_key</b> $var_cfg_error ($nagMapR_key)";
 
 if(!is_int($nagMapR_Debug))
-  die("\$nagMapR_Debug $var_cfg_error ($nagMapR_Debug)");
+  $fails .= "<br><b>\$nagMapR_Debug</b> $var_cfg_error ($nagMapR_Debug)";
 
 if(!is_int($nagMapR_DateFormat))
-  die("\$nagMapR_DateFormat $var_cfg_error ($nagMapR_DateFormat)");
+  $fails .= "<br><b>\$nagMapR_DateFormat</b> $var_cfg_error ($nagMapR_DateFormat)";
 
 if(!is_int($nagMapR_PlaySound))
-  die("\$nagMapR_PlaySound $var_cfg_error ($nagMapR_PlaySound)");
+  $fails .= "<br><b>\$nagMapR_PlaySound</b> $var_cfg_error ($nagMapR_PlaySound)";
 
 if(!is_int($nagMapR_ChangesBar))
-  die("\$nagMapR_ChangesBar $var_cfg_error ($nagMapR_ChangesBar)");
+  $fails .= "<br><b>\$nagMapR_ChangesBar</b> $var_cfg_error ($nagMapR_ChangesBar)";
 
 if(($nagMapR_ChangesBarMode < 1) || ($nagMapR_ChangesBarMode > 2))
-  die("\$nagMapR_ChangesBarMode $var_cfg_error ($nagMapR_ChangesBarMode)");
+  $fails .= "<br><b>\$nagMapR_ChangesBarMode</b> $var_cfg_error ($nagMapR_ChangesBarMode)";
 
-if(($nagMapR_Reporting < 0) || ($nagMapR_Reporting > 1))
-  die("\$nagMapR_Reporting $var_cfg_error ($nagMapR_Reporting)");
+if(($nagMapR_Reporting < 0) || ($nagMapR_Reporting > 1)  || (!isset($nagMapR_Reporting)))
+  $fails .= "<br><b>\$nagMapR_Reporting</b> $var_cfg_error ($nagMapR_Reporting)";
 
 if(!is_int($nagMapR_Lines))
-  die("\$nagMapR_Lines $var_cfg_error ($nagMapR_Lines)");
+  $fails .= "<br><b>\$nagMapR_Lines</b> $var_cfg_error ($nagMapR_Lines)";
 
 if(!( is_float($nagMapR_ChangesBarSize) || is_int($nagMapR_ChangesBarSize) ))
-  die("\$nagMapR_ChangesBarSize $var_cfg_error ($nagMapR_ChangesBarSize)");
+  $fails .= "<br><b>\$nagMapR_ChangesBarSize</b> $var_cfg_error ($nagMapR_ChangesBarSize)";
 
 if(!( is_float($nagMapR_FontSize) || is_int($nagMapR_FontSize) ))
-  die("\$nagMapR_FontSize $var_cfg_error ($nagMapR_FontSize)");
+  $fails .= "<br><b>\$nagMapR_FontSize</b> $var_cfg_error ($nagMapR_FontSize)";
 
 if(!( is_float($nagMapR_MapZoom) || is_int($nagMapR_MapZoom) ))
-  die("\$nagMapR_MapZoom $var_cfg_error ($nagMapR_MapZoom)");
+  $fails .= "<br><b>\$nagMapR_MapZoom</b> $var_cfg_error ($nagMapR_MapZoom)";
 
 if(!( is_float($nagMapR_TimeUpdate) || is_int($nagMapR_TimeUpdate) ))
-  die("\$nagMapR_TimeUpdate $var_cfg_error ($nagMapR_TimeUpdate)");
+  $fails .= "<br><b>\$nagMapR_TimeUpdate</b> $var_cfg_error ($nagMapR_TimeUpdate)";
 
-if($nagMapR_IconStyle > 2 || $nagMapR_IconStyle < 0)
-  die("\$nagMapR_IconStyle $var_cfg_error ($nagMapR_IconStyle)");
+if($nagMapR_IconStyle > 2 || $nagMapR_IconStyle < 0  || (!isset($nagMapR_IconStyle)))
+  $fails .= "<br><b>\$nagMapR_IconStyle</b> $var_cfg_error ($nagMapR_IconStyle)";
+
+if(empty($nagMapR_User) || empty($nagMapR_UserKey))
+  $fails .= "<br>$emptyUserPass";
 
 if(!extension_loaded('mbstring'))
-  die("$moduleError mbstring");
+  $fails .= "<br>$moduleError mbstring";
 
 if(!extension_loaded('json'))
-  die("$moduleError json");
+  $fails .= "<br>$moduleError json";
+
+if(!empty($fails))
+  die($fails);
 
 function checkUserPass(){
   include('config.php');
